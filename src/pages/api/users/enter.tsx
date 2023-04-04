c247c45da380cd0c8a7ccbae56d5422759fc6b78
read0more <read0more@gmail.com>
2023-04-04 18:08:59 +0900

    :sparkles: enter user with SMS(twilio)

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,10 +1,54 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import client from 'libs/server/client';
-import withHandler from 'libs/server/withHandler';
+import withHandler, { ResponseType } from 'libs/server/withHandler';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.status(200).end();
+import twilio from 'twilio';
+
+type Data = {
+  name: string;
+};
+
+const twilioClient = twilio(
+  process.env.TWILLO_ACCOUNT_SID,
+  process.env.TWILLO_TOKEN
+);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: 'Anonymous',
+            ...user,
+          },
+        },
+      },
+    },
+  });
+
+  if (phone) {
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILLO_MESSAGE_SID,
+      to: process.env.TWILLO_PHONE_NUMBER!,
+      body: `Your Carrot verification code is ${payload}`,
+    });
+  }
+
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler('POST', handler);
