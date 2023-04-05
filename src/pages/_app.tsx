c2a9c73836cd935532a281416da2d73d562eabc8
read0more <read0more@gmail.com>
2023-04-05 17:57:28 +0900

    :sparkles: user hook with swr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // <div className="mx-auto w-full max-w-xl">
-    <div className="h-full min-h-screen bg-green-100 py-10">
-      <Component {...pageProps} />
-    </div>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
+      }}
+    >
+      <div className="h-full min-h-screen bg-green-100 py-10">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
   );
 }
--- src/pages/api/users/enter.ts
+++ src/pages/api/users/enter.ts
@@ -70,4 +70,4 @@ async function handler(
   });
 }
 
-export default withHandler('POST', handler);
+export default withHandler({ method: 'POST', handler, isPrivate: false });
