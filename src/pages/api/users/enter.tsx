ebb8f61416a13cd45cab64a87d7aab66fbc0e24b
read0more <read0more@gmail.com>
2023-04-05 11:24:06 +0900

    :sparkles: enter user with E-mail(nodemailer)

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,13 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import client from 'libs/server/client';
 import withHandler, { ResponseType } from 'libs/server/withHandler';
-
 import twilio from 'twilio';
+import nodemailer from 'nodemailer';
 
 const twilioClient = twilio(
   process.env.TWILLO_ACCOUNT_SID,
   process.env.TWILLO_TOKEN
 );
+
+const transporter = nodemailer.createTransport({
+  service: 'Naver',
+  host: 'smtp.naver.com',
+  port: 587,
+  auth: {
+    user: process.env.NAVER_MAIL_ID,
+    pass: process.env.NAVER_MAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 async function handler(
   req: NextApiRequest,
@@ -35,11 +48,21 @@ async function handler(
   });
 
   if (phone) {
-    await twilioClient.messages.create({
+    const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILLO_MESSAGE_SID,
       to: process.env.TWILLO_PHONE_NUMBER!,
       body: `Your Carrot verification code is ${payload}`,
     });
+    console.log(message);
+  } else if (email) {
+    const message = await transporter.sendMail({
+      from: process.env.NAVER_MAIL,
+      to: email,
+      subject: 'Your Carrot Market Verification Email',
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(message);
   }
 
   return res.json({
