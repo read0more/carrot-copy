f94f251677a76c5dbdaeb05dab022c66ac26835d
read0more <read0more@gmail.com>
2023-04-09 19:26:38 +0900

    :sparkles: stream messages

--- src/pages/api/streams/[id]/messages.ts
+++ src/pages/api/streams/[id]/messages.ts
@@ -9,18 +9,30 @@ async function handler(
 ) {
   const {
     query: { id },
+    body,
+    session: { user },
   } = req;
-  const stream = await client.stream.findUnique({
-    where: {
-      id: +id!,
+  const message = await client.message.create({
+    data: {
+      message: body.message,
+      stream: {
+        connect: {
+          id: +id.toString(),
+        },
+      },
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
     },
   });
-  res.json({ ok: true, stream });
+  res.json({ ok: true, message });
 }
 
 export default withApiSession(
   withHandler({
-    methods: ['GET'],
+    methods: ['POST'],
     handler,
   })
 );
